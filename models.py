cc1069203c58989a1fb2d56d6734e1b390389ec3
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-06-28 12:31:55 +0530

    Added UserCreate

--- models.py
+++ models.py
@@ -26,6 +26,10 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    password: Optional[str] = None
+
 class User(UserBase):
     id: int
     is_active: bool
